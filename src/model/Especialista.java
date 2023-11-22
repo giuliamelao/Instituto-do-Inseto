c5c55d527eef0a06335aa38d97ae37af03d5ce5a
Giulia Melão <106086930+giuliamelao@users.noreply.github.com>
2023-11-22 10:31:25 -0300

    Add files via upload

---
+++ src/model/Especialista.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.*;
+
+/**
+ *
+ * @author Giulia
+ */
+public class Especialista {
+    static public String nome;
+    static public String login;
+    static public int senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+}
---
+++ src/model/Inseto.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Giulia
+ */
+public class Inseto {
+    public String nome_pop;
+    public String nome_cien;
+    public int qtde_olhos;
+    public int qtde_pernas;
+    public String cor;
+    public boolean peconhento;
+    public String pais;
+    public String estado;
+    public String nome_especialista;
+    public String data_cadastro;
+
+    public String getNome_pop() {
+        return nome_pop;
+    }
+
+    public void setNome_pop(String nome_pop) {
+        this.nome_pop = nome_pop;
+    }
+
+    public String getNome_cien() {
+        return nome_cien;
+    }
+
+    public void setNome_cien(String nome_cien) {
+        this.nome_cien = nome_cien;
+    }
+
+    public int getQtde_olhos() {
+        return qtde_olhos;
+    }
+
+    public void setQtde_olhos(int qtde_olhos) {
+        this.qtde_olhos = qtde_olhos;
+    }
+
+    public int getQtde_pernas() {
+        return qtde_pernas;
+    }
+
+    public void setQtde_pernas(int qtde_pernas) {
+        this.qtde_pernas = qtde_pernas;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public boolean isPeconhento() {
+        return peconhento;
+    }
+
+    public void setPeconhento(boolean peconhento) {
+        this.peconhento = peconhento;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNome_especialista() {
+        return nome_especialista;
+    }
+
+    public void setNome_especialista(String nome_especialista) {
+        this.nome_especialista = nome_especialista;
+    }
+
+    public String getData_cadastro() {
+        return data_cadastro;
+    }
+
+    public void setData_cadastro(String data_cadastro) {
+        this.data_cadastro = data_cadastro;
+    }
+}
